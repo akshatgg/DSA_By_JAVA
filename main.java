367e38d9e9fd97e0ab1200de93c8ac06074a21f6
akshatgg <akshatg9636@gmail.com>
2023-04-12 02:38:29 +0530

    class

---
+++ main.java
@@ -0,0 +1,18 @@
+
+public class main{
+   
+    public static void main(String [] args){
+  pokemon p1=new pokemon();
+  pokemon p2=new pokemon();
+  pokemon p3=new pokemon("charazard",33);
+  p1.name="pikachuu";
+  p1.level=10;
+  p2.name="eve";
+  p2.level=20;
+  p2.attack();
+  System.out.println(p1.age);
+  System.out.println(p3.level);
+    }
+   
+
+}
---
+++ pokemon.java
@@ -0,0 +1,15 @@
+public class pokemon {
+    String name;
+    int level;
+    int age;
+    void attack(){
+        System.out.println(name+"attack");
+    }
+    pokemon(){
+        age=1;
+    }
+    pokemon(String pname,int plevel){
+name=pname;
+level=plevel;
+    }
+}
