2d9646f2b7ec4289dcfdac75f26dda72267b0949
akshatgg <akshatg9636@gmail.com>
2023-04-29 17:44:19 +0530

    dsa

---
+++ DoublyLinkedList.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class DoublyLinkedList {
+    public static void main(String[] args){
+        DoublyLinkedListClass mydl=new DoublyLinkedListClass(4);
+        mydl.printlist();
+        mydl.getlength();
+    }
+}
