e42a333cb442933d04d310a9c80f50abc40efb8d
akshatgg <akshatg9636@gmail.com>
2023-05-25 01:32:45 +0530

    dsa

---
+++ DLLSwaptwoPairs.java
@@ -0,0 +1,39 @@
+public class DLLSwaptwoPairs {
+    
+
+        public static void main(String[] args) {
+            
+            DLLSwaptwoPairsClass myDll = new DLLSwaptwoPairsClass(1);
+            myDll.append(2);
+            myDll.append(3);
+            myDll.append(4);
+            System.out.println("myDll before swapPairs:");
+
+            myDll.printList();
+
+            myDll.swapPairs();
+    
+            System.out.println("\nmyDll after swapPairs:");
+            myDll.printList();
+    
+            /*
+                EXPECTED OUTPUT:
+                ----------------
+                myDll before swapPairs:
+                1
+                2
+                3
+                4
+                
+                myDll after swapPairs:
+                2
+                1
+                4
+                3
+    
+            */
+    
+        }
+    
+    }
+
---
+++ DLLSwaptwoPairsClass.java
@@ -0,0 +1,124 @@
+public class DLLSwaptwoPairsClass {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DLLSwaptwoPairsClass(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+    
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.prev = current;
+        }
+        length++;
+    }
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        if (index < length/2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+public Node swapPairs(){
+    if(length%2!=0){
+    for(int i=0;i<length;i++){
+        if(i%2==0){
+        int temp=get(i).value;       
+        get(i).value=get(i+1).value;
+        get(i+1).value=temp;
+        }   
+    }
+    }
+else if(length%2==0){
+    for(int i=0;i<length;i++){
+        if(i%2!=0){
+            int temp=get(i).value;
+            get(i).value=get(i+1).value;
+            get(i+1).value=temp;
+        }
+    }
+}
+    return head;
+}
+    // WRITE THE SWAPPAIRS METHOD HERE // 
+    //                                 //
+    //                                 //
+    //                                 //
+    //                                 //
+    /////////////////////////////////////
+
+}
