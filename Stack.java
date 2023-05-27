92606e0f492530cd05ed265bd5742146ce54bae5
akshatgg <akshatg9636@gmail.com>
2023-05-27 20:08:43 +0545

    stack

--- Stack.java
+++ Stack.java
@@ -1,8 +1,11 @@
 public class Stack {
     public static void main(String[] args){
         StackClass SC=new StackClass();
-        SC.Stack(5);
-     SC.height();
+        SC.Stack(8);
+    //  SC.height();
+     SC.push(3);
+     SC.push(4);
+     SC.pop();
      SC.printlist();
     }
     
--- StackClass.java
+++ StackClass.java
@@ -4,6 +4,7 @@ public class StackClass {
     class Node{
         int value;
         Node next;
+        Node prev;
         Node(int value){
             this.value=value;
         }
@@ -23,5 +24,36 @@ public void printlist(){
 }
 public void height(){
     System.out.println(height);
+}
+public void push(int value){
+    Node newNode=new Node(value);
+
+    Node temp=top;
+    if(height==0){
+        top=newNode;
+        height=1;
+    }
+    else{
+        temp.prev=newNode; 
+                            //  M2   //  newNode.next=top
+                                    //   top=newNode;
+        
+        newNode.next=top;
+        top=temp.prev;           
+        top.next=temp;
+    }
+    height++; 
+}
+public Node pop(){
+    if(height==0){
+    top=null;
+    }
+    else{
+        Node temp=top;
+                top=top.next;
+                temp.next=null;
+    }
+    height--;
+    return top;
 }
 }
