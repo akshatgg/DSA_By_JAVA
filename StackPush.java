e1b36fc2cf3f2df82601405a889b463dd9788ecd
akshatgg <akshatg9636@gmail.com>
2023-05-28 21:03:29 +0545

    STACK

---
+++ StackPush.java
@@ -0,0 +1,25 @@
+public class StackPush {
+    
+
+        public static void main(String[] args) {
+    
+            StackPushClass myStack = new StackPushClass();
+            
+            myStack.push(1);
+            myStack.push(2);
+            myStack.push(3);
+            
+            myStack.printStack();
+            
+            /*
+                EXPECTED OUTPUT:
+                3
+                2
+                1
+    
+            */
+    
+        }
+    
+    }
+
