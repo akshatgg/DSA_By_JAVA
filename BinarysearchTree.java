4c69428e6897b14abc6d67706d2abd2604a1d3d7
akshatgg <akshatg9636@gmail.com>
2023-05-28 13:03:34 +0545

    bst

---
+++ BinarysearchTree.java
@@ -0,0 +1,6 @@
+public class BinarysearchTree {
+    public static void main(String[] args){
+        BinarysearchTreeClass mybst= new BinarysearchTreeClass(); 
+    System.out.println(mybst.root);
+    }
+}
---
+++ BinarysearchTreeClass.java
@@ -0,0 +1,12 @@
+public class BinarysearchTreeClass {
+     Node root;
+     class Node{
+        int value;
+        Node left;
+        Node right;
+        private Node(int value) 
+        {
+       this.value = value;
+     }
+    }
+}
