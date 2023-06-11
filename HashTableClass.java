ad2343ad21755cd0abda0c15c9410f3e86a150a6
akshatgg <akshatg9636@gmail.com>
2023-06-11 23:52:52 +0545

    hashtable

---
+++ HashTableClass.java
@@ -0,0 +1,33 @@
+public class HashTableClass {
+    private int size=7;
+    private Node[] datamap;
+    class Node{
+        String key;
+        Node next;
+        int value;
+        Node(String key,int value){
+         this.key=key;
+         this.value=value;
+        }
+    }
+    public void HashTableClass(){
+        datamap=new Node[size];
+    }
+    public void printall(){
+        for(int i=0;i<datamap.length;i++){
+          while(datamap[i]!=null){
+            System.out.println(datamap[i].key + "="+datamap[i].value);
+            datamap[i]=datamap[i].next;
+          }
+        }
+    }
+    public int hash(String key){
+      int hash=0;
+      char[] k=key.toCharArray();
+      for(int i=0;i<datamap.length;i++){
+        int asciiValue=k[i];
+        hash=(hash+asciiValue*23)%datamap.length;
+    }
+    return hash;
+}
+}
