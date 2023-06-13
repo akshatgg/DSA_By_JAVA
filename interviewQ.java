2a9fa5e1eb9762ba747101aaf0a2e3f503ff27a0
akshatgg <akshatg9636@gmail.com>
2023-06-13 13:07:51 +0545

    interview

--- interviewQ.java
+++ interviewQ.java
@@ -1,3 +1,19 @@
+import java.util.HashMap;
+
 public class interviewQ {
-    
+    public static boolean itemcommon(int[] A,int[] B){
+        HashMap<Integer,Boolean> myhash=new HashMap<>();
+        for(int i:A){
+myhash.put(i, true);
+        }
+        for(int j:B){
+            if(myhash.get(j)!=null)return true;
+        }
+        return false;
+    }
+    public static void main(String []args){
+        int[] A={1,2,4};
+        int[] B={5,6,4};
+        System.out.println(itemcommon(A,B));
+    }
 }
