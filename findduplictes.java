f9ec5d420d9b13905eb24c0d4c74b5ab79f84938
akshatgg <akshatg9636@gmail.com>
2023-06-17 09:52:58 +0545

    duplicate

--- findduplictes.java
+++ findduplictes.java
@@ -1,17 +1,26 @@
 import java.util.*;
 public class findduplictes {
-    public static int findDuplicates(int[] nums){
+    public static Set<Integer> findDuplicates(int[] nums){
 HashMap<Integer,Integer> myhp=new HashMap<>();
-int count=0;
 for(int e:nums){
-  if(myhp.containsKey(e)){
-count++;
+  int count=0;
+  for(int m:nums){
+    if(e==m){
+      count++;
+    }
   }
+  if(count>=2){
+    myhp.put(e,count);
+  }
+//   if(myhp.containsKey(e)){
+// count++;
+  }
+return myhp.keySet();
 }
 
 
 
-    }
+    
     // WRITE THE FINDDUPLICATES METHOD HERE // 
     //                                      //
     //                                      //
@@ -23,7 +32,7 @@ count++;
     public static void main(String[] args) {
 
         int[] nums = {1, 2, 3, 2, 1, 4, 5, 4};
-        List<Integer> duplicates = findDuplicates(nums);
+        Set<Integer> duplicates = findDuplicates(nums);
         System.out.println(duplicates); 
         
         /*
