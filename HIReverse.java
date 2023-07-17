f0c5335b0a558d09db9cc5a66a116f11b7399fe4
akshatgg <akshatg9636@gmail.com>
2023-07-17 23:54:36 +0545

    modify

--- HIReverse.java
+++ HIReverse.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import java.util.*;
-
-
 public class HIReverse {
-
     static class SinglyLinkedListNode {
         public int data;
         public SinglyLinkedListNode next;
@@ -13,43 +10,32 @@ public class HIReverse {
             this.next = null;
         }
     }
-
     static class SinglyLinkedList {
         public SinglyLinkedListNode head;
         public SinglyLinkedListNode tail;
-
         public SinglyLinkedList() {
             this.head = null;
             this.tail = null;
         }
-
         public void insertNode(int nodeData) {
             SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
-
             if (this.head == null) {
                 this.head = node;
             } else {
                 this.tail.next = node;
             }
-
             this.tail = node;
         }
     }
-
     public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
         while (node != null) {
             bufferedWriter.write(String.valueOf(node.data));
-
             node = node.next;
-
             if (node != null) {
                 bufferedWriter.write(sep);
             }
         }
     }
-
-
-
     /*
      * Complete the 'reverse' function below.
      *
